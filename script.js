a4306f2bed79c8a035574479912cb2e396917b92
Gust4v0M <gustavomorgado6@gmail.com>
2023-01-04 01:52:41 -0300

    script done

--- script.js
+++ script.js
@@ -4,14 +4,72 @@
 
 // Crianças valem por 0,5
 
-let adultos = document.getElementById("adultos")
+let inputAdultos = document.getElementById("adultos");
 
-let criancas = document.getElementById("criancas")
+let inputCriancas = document.getElementById("criancas");
 
-let tempo = document.getElementById("duracao")
+let inputTempo = document.getElementById("duracao");
 
-let result = document.getElementById("calcula")
+let result = document.getElementById("calcula");
 
-let square = document.getElementsByClassName("square")
+let escrita = document.getElementById("texto");
 
 
+function calcula() {
+  let adultos = inputAdultos.value;
+
+  let criancas = inputCriancas.value;
+
+  let tempo = inputTempo.value;
+
+
+
+      var qntTotalCarne = carnePP(tempo) * adultos + (carnePP(tempo) / 2) * criancas;
+
+      var qntTotalCerveja = brejaPP(tempo) * adultos 
+
+      var qntTotalBebidas = carnePP(tempo) * adultos + (carnePP(tempo) / 2) * criancas;
+
+      escrita.innerHTML =
+      `<h2> Você vai preciar de</h2>`
+        
+      escrita.innerHTML = 
+      `<p>${qntTotalCarne / 1000} Kg de carne</p>` 
+
+      escrita.innerHTML += 
+      ` <p>${Math.ceil(qntTotalCerveja / 350)} latinhas de cerveja</p>`
+
+      escrita.innerHTML += 
+      ` <p>${qntTotalBebidas / 1000} litros de suco</p>`
+      
+}
+
+function carnePP(tempo) {
+  if (tempo >= 6) {
+    return 650;
+  } else {
+    return 400;
+  }
+}
+
+function brejaPP(tempo) {
+  if (tempo >= 6) {
+    return 2000;
+  } else {
+    return 1200;
+  }
+}
+
+function bebidasPP(tempo) {
+    if (tempo >= 6) {
+      return 1000;
+    } else {
+      return 1500;
+    }
+  }
+
+
+// carnePP();
+// brejaPP();
+// bebidasPP();
+// calcula();
